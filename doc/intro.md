608b8072db39b2017a52aa337ea114510b884b77
murilinhops <murilo.pescatori@nubank.com.br>
2024-06-12 16:19:00 -0300

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ace-of-cards
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/murilinhops/ace_of_cards/core_test.clj
@@ -0,0 +1,7 @@
+(ns murilinhops.ace-of-cards.core-test
+  (:require [clojure.test :refer :all]
+            [murilinhops.ace-of-cards.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
