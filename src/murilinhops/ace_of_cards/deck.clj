b09fc9b1a7c64b19968088237d88ba4d7e91cd20
murilinhops <murilo.pescatori@nubank.com.br>
2024-06-13 17:42:41 -0300

    feat: can take card from deck, and put it on hand or discard-pile

--- src/murilinhops/ace_of_cards/deck.clj
+++ src/murilinhops/ace_of_cards/deck.clj
@@ -2,14 +2,14 @@
   (:require [murilinhops.ace-of-cards.card :as card]
             [murilinhops.ace-of-cards.utils :as utils]
             [schema.core :as s]))
-
-(s/defschema Deck {:deck [card/Card] :discard-pile [card/Card]})
+;A M B U CARALHO
+(s/defschema Game {:deck [card/Card] :hand [card/Card] :discard-pile [card/Card]})
 
 (s/defn insert-jokers [deck]
   (let [joker {:rank "Joker" :suit :joker}]
     (conj deck joker joker)))
 
-(s/defn create-deck :- Deck
+(s/defn create-game :- Game
   [ace-of-spades? :- s/Bool]
   (let [rank-limit (+ 1 (utils/rank-limit ace-of-spades?))
         cards (for [suit  card/suits ;tipo generateList(lenght(13), data: {suit, rank})
@@ -17,7 +17,24 @@
                     rank  (range 1 rank-limit)]
                 (card/create-card ace-of-spades? suit rank))
         cards-including-joker (insert-jokers cards)]
-    {:deck cards-including-joker :discard-pile []}))
+    {:hand []
+     :deck cards-including-joker
+     :discard-pile []}))
 
-(defn shuffle-deck [deck]
-  (update deck :deck shuffle))
+(s/defn take-card-from-deck
+  [game :- Game 
+   coll :- s/Keyword]
+  (if (empty? (:deck game))
+    game ;TODO: add the discard-pile to the deck and shuffle it again
+    (let [card (first (:deck game))
+          updated-deck (update game :deck rest) ; !rest retorna todos os elementos da lista tirando o primeiro
+          updated-game (update updated-deck coll conj card)]
+     updated-game)))
+
+(defn take-cards-x-times-from-deck [deck n]
+  (reduce (fn [current-deck _] (take-card-from-deck current-deck :hand)) 
+          deck 
+          (range n)))
+
+(defn shuffle-deck [game]
+  (update game :deck shuffle))
