39d9e5d54d660278b597faaace831fca7462f87a
murilinhops <murilinhorps@gmail.com>
2024-06-15 13:18:08 -0300

    feat: create skeleton for player skills

---
+++ src/murilinhops/ace_of_cards/skills.clj
@@ -0,0 +1,14 @@
+(ns murilinhops.ace-of-cards.skills
+  (:require [murilinhops.ace-of-cards.card :as card]
+            [murilinhops.ace-of-cards.game :as game]
+            [murilinhops.ace-of-cards.utils :as utils]
+            [murilinhops.ace-of-cards.actions :as actions]
+            [schema.core :as s]))
+
+(defn magic-cards "Resolve cards" [])
+
+(defn high-or-low [])
+
+(defn mulligan [])
+
+(defn use-trap-card [])
