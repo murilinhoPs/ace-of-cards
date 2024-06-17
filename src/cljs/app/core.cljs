85c54399ed0fcdb6ad5454f0774a94181f5c7b30
murilinhops <murilinhorps@gmail.com>
2024-06-17 12:33:59 -0300

    .

--- src/cljs/app/core.cljs
+++ src/cljs/app/core.cljs
@@ -7,16 +7,18 @@
 ;; define components using the `defnc` macro
 (defnc greeting
   "A component which greets a user."
-  [{:keys [name]}]
+  [{:keys [name color]}]
    ;; use helix.dom to create DOM elements
-  (d/div "Hello, " (d/strong name) "!"))
+  (d/div {:style {:color color}}
+   "Hello, " (d/strong name) "!"))
 
 (defnc app []
   (let [[state set-state] (hooks/use-state {:name "Helix User"})]
     (d/div
      (d/h1 "Welcome!")
      ;; create elements out of components
-     ($ greeting {:name (:name state)})
+     ($ greeting {:name (:name state)
+                  :color "green"})
      (d/input {:value (:name state)
                :on-change #(set-state assoc :name (.. % -target -value))}))))
 
