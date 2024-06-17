7bd6ab39886b568453dec045dee17d065c8b1f9d
murilinhops <murilinhorps@gmail.com>
2024-06-17 18:08:35 -0300

    refac: organize code

--- src/cljs/app/core.cljs
+++ src/cljs/app/core.cljs
@@ -1,23 +1,38 @@
 (ns cljs.app.core
   (:require ["react-dom/client" :as rdom]
-            [helix.core :refer [$ defnc]]
+            [helix.core :refer [$ defnc <>]]
             [helix.dom :as d]
             [helix.hooks :as hooks]
             [clj.ace-of-cards.core :refer [new-game]]))
 
-; show the hand (a list of cards)
+(defn _start-game [set-state]
+  (let [data (new-game true)]
+    (set-state assoc :hand (:hand data))
+    (set-state assoc :deck (:deck data))
+    (set-state assoc :discard-pile (:discard-pile data))))
+
+(defn hand-cards [hand]
+  (when (< 0 (count hand))
+    (d/div {:style {:display "flex" 
+                    :flex-direction "column"}}
+           (for [card hand]
+             (d/div {:style {:padding "8px 0px"
+                             :display "flex"
+                             :column-gap "16px"}}
+                    (if (= :joker (:suit card))
+                      (d/text (:rank card))
+                      (<> (d/text (str "Valor: " (:rank card)))
+                          (d/text (str "Naipe: " (:suit card))))))))))
 
 (defnc app []
-  (let [[state set-state] (hooks/use-state {:hand []})
-        start-game (fn [] (let [data (new-game true)]
-                            (println data)
-                            (println (:hand data))
-                            (set-state assoc :hand (:hand data))))]
+  (let [[state set-state] (hooks/use-state {:hand [], :deck [], :discard-pile []})
+        start-game (fn [] (_start-game set-state))]
     (d/div
-     (d/h1 "Ace of Cards - Fabula Ultima")
-     (d/input {:value (:hand state)
-               :on-change #(set-state assoc :hand (.. % -target -value))})
-     (d/button  {:on-click #(start-game)} "New Game"))))
+     (d/header {:class "header"}
+               (d/h1 "Ace of Cards - Fabula Ultima"))
+     (hand-cards (:hand state))
+     (d/button  {:class "start-button", :on-click #(start-game)}
+                (if (-> state :deck empty?) "New Game" "Reset Game")))))
 
 (defonce root (rdom/createRoot (js/document.getElementById "app")))
 
