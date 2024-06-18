61f66becc92cbcc058e3a41cbbbf46caae4057d3
murilinhops <murilinhorps@gmail.com>
2024-06-18 16:09:44 -0300

    feat: create deck and discard-pile components

--- src/cljs/app/core.cljs
+++ src/cljs/app/core.cljs
@@ -71,7 +71,6 @@
                   :flex-direction "column"
                   :width "7rem"
                   :height "9rem"
-                  :position "relative"
                   :box-shadow "-8px 10px 0 -2.5px var(--main-bg-color), 
                                -12px 12px 0 0 var(--main-color), 
                                -18px 20px 0 0 var(--main-bg-color), 
@@ -86,10 +85,8 @@
                   :justify-content "center"
                   :align-items "center"
                   :display "flex"
-                  :flex-direction "column"
                   :width "7rem"
-                  :height "9rem"
-                  :position "relative"
+                  :height "9rem" 
                   :box-shadow "-8px 10px 0 -2.5px var(--main-bg-color), 
                                -12px 12px 0 0 var(--secondary-color), 
                                -18px 20px 0 0 var(--main-bg-color), 
@@ -101,7 +98,8 @@
     (d/div {:style {:display "flex"
                     :flex-direction "row"
                     :flex-wrap "wrap"
-                    :gap "16px"}}
+                    :gap "16px"
+                    :align-self "start"}}
            (for [card hand]
              (d/div {:style {:padding "8px 0px"
                              :display "flex"
@@ -116,7 +114,7 @@
      (d/header {:class "header" :style {:display "flex"
                                         :justify-content "space-between"
                                         :align-items "center"
-                                        :margin "12px"}}
+                                        :margin "12px 12px 48px"}}
                (d/h1 "Ace of Cards - Fabula Ultima")
                (d/button  {:id "start-button"
                            :on-click #(start-game);TODO: criar alerta de confirmação quando for reiniciar
@@ -132,13 +130,17 @@
                                    :min-width "120px"}}
                           (if empty-state? "New Game" "Reset Game")))
      (when (not empty-state?)
-       (d/main
+       (d/main {:style {:display "flex" :justify-content "space-between" :align-items "center"}}
         (hand-cards (:hand state))
-        (d/aside {:style {:position "relative"}}
-                 (d/div {:style {:position "absolute", :bottom "28px", :right "20px"}}
-                        ($ discard-pile-component))
-                 (d/div {:style {:position "absolute", :bottom "456px", :right "20px"}}
-                        ($ deck-component))))))))
+        (d/aside {:style {:display "flex"
+                          :align-items "center"
+                          :justify-content "center"
+                          :flex-direction "column"
+                          :align-self "center" 
+                          :height "80vh"
+                          :gap "240px"}}
+                 ($ deck-component)
+                 ($ discard-pile-component)))))))
 
 (defonce root (rdom/createRoot (js/document.getElementById "app")))
 
