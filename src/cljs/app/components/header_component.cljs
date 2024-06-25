f4060a65ad17574a0e34f2e000a442e9b271139b
murilinhops <murilinhorps@gmail.com>
2024-06-25 14:58:07 -0300

    feat: add translations on header

--- src/cljs/app/components/header_component.cljs
+++ src/cljs/app/components/header_component.cljs
@@ -1,40 +1,46 @@
 (ns app.components.header-component
-  (:require [helix.core :refer [defnc]]
-            [helix.dom :as d]))
+  (:require
+   [app.i18n]
+   [helix.core :refer [defnc]]
+   [helix.dom :as d]))
 
 (defnc header-component [{:keys [started? start-game restart-game shuffle-deck]}]
-  (d/header {:class "header"
-             :style {:display "flex"
-                     :justify-content "space-between"
-                     :align-items "center"
-                     :margin "12px 12px 48px"}}
-            (d/h1 "Ace of Cards - Fabula Ultima")
-            (d/div {:style {:display "flex"
-                            :flex-direction "row"
-                            :flex-wrap "wrap"
-                            :gap "16px"
-                            :align-self "center"}}
-                   (when started? (d/button
-                                   {:id "header-button"
-                                    :on-click #(shuffle-deck)
-                                    :style {:background-color "var(--secondary-color)"
-                                            :color "var(--text-color)"
-                                            :border "none"
-                                            :border-radius "12px"
-                                            :padding "8px"
-                                            :font-weight "bold"
-                                            :max-height "36px"
-                                            :min-width "120px"}}
-                                   "Shuffle Deck"))
-                   (d/button
-                    {:id "header-button"
-                     :on-click (if started? #(restart-game) #(start-game))
-                     :style {:background-color  (if started? "var(--main-color)" "var(--secondary-color)")
-                             :color "var(--text-color)"
-                             :border "none"
-                             :border-radius "12px"
-                             :padding "8px"
-                             :font-weight "bold"
-                             :max-height "36px"
-                             :min-width "120px"}}
-                    (if started? "Reset Game" "New Game")))))
+  (print (.-language js/navigator))
+  (let [shuffle-deck' (app.i18n/app-tr [:header/shuffle-deck])
+        new-game'     (app.i18n/app-tr [:header/new-game])
+        reset-game'   (app.i18n/app-tr [:header/reset-game])]
+    (d/header {:class "header"
+               :style {:display "flex"
+                       :justify-content "space-between"
+                       :align-items "center"
+                       :margin "12px 12px 48px"}}
+              (d/h1 "Ace of Cards - Fabula Ultima")
+              (d/div {:style {:display "flex"
+                              :flex-direction "row"
+                              :flex-wrap "wrap"
+                              :gap "16px"
+                              :align-self "center"}}
+                     (when started? (d/button
+                                     {:id "header-button"
+                                      :on-click #(shuffle-deck)
+                                      :style {:background-color "var(--secondary-color)"
+                                              :color "var(--text-color)"
+                                              :border "none"
+                                              :border-radius "12px"
+                                              :padding "8px"
+                                              :font-weight "bold"
+                                              :max-height "36px"
+                                              :min-width "120px"}}
+                                     shuffle-deck'))
+                     (d/button
+                      {:id "header-button"
+                       :on-click (if started? #(restart-game) #(start-game))
+                       :style {:background-color  (if started? "var(--main-color)" "var(--secondary-color)")
+                               :color "var(--text-color)"
+                               :border "none"
+                               :border-radius "12px"
+                               :padding "8px"
+                               :font-weight "bold"
+                               :max-height "36px"
+                               :min-width "120px"}}
+                      (if started? reset-game' new-game'))))))
